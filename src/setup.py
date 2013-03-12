5734479bc031ae3c1ccf945f12e1669c4636b0af
Friedrich Paetzke <f.paetzke@gmail.com>
2013-03-12 21:52:26 +0000

    add: install script

---
+++ src/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='pyterm',
+      description='some highlighting functions for terminals',
+      version='0.0.1',
+      author='Friedrich Paetzke',
+      author_email='f.paetzke@gmail.com',
+      py_modules=['pyterm'],
+  )
