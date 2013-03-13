b0567226b076fea76e17708c4980f34729f3fb5d
Friedrich Paetzke <f.paetzke@gmail.com>
2013-03-13 15:30:22 +0000

    updated version

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='pyterm',
       description='some highlighting functions for terminals',
-      version='0.0.1',
+      version='0.0.2',
       author='Friedrich Paetzke',
       author_email='f.paetzke@gmail.com',
       py_modules=['pyterm'],
