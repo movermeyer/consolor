f16623ebb13347a71c809a3403d0f2c53c3d721b
Friedrich Paetzke <f.paetzke@gmail.com>
2013-12-07 13:54:31 +0100

    Bump version: 0.0.0 → 0.0.1

--- consolor/__init__.py
+++ consolor/__init__.py
@@ -7,7 +7,7 @@ All rights reserved.
 """
 from .consolor import get_line, print_line, update_line, BgColor, Color
 
-__version__ = '0.0.0'
+__version__ = '0.0.1'
 __author__ = 'Friedrich Paetzke'
 __license__ = 'BSD'
 __copyright__ = 'Copyright 2013 Friedrich Paetzke'
