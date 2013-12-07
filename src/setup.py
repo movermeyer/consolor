ad7d43c3992d8f11bee8817866afdf7f6559b026
Friedrich Paetzke <f.paetzke@gmail.com>
2013-12-07 13:08:17 +0100

    Remove old files.

--- src/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(name='pyterm',
-      description='some highlighting functions for terminals',
-      version='0.0.2',
-      author='Friedrich Paetzke',
-      author_email='f.paetzke@gmail.com',
-      py_modules=['pyterm'],
-  )
