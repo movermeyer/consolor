66bbf6659436908ee9f0626857cbebe0a01fc24e
Friedrich Paetzke <f.paetzke@gmail.com>
2014-01-20 22:15:33 +0100

    Use functions and =assert= instead of =TestCase=

--- tests/test_consolor.py
+++ tests/test_consolor.py
@@ -7,7 +7,6 @@ All rights reserved.
 
 """
 from __future__ import print_function
-import unittest
 
 import consolor
 
@@ -21,78 +20,85 @@ def mockable_print(*args, **kwargs):
     print(*args, **kwargs)
 
 
-class TestConsolor(unittest.TestCase):
+def test_print_bold():
+    result = consolor.get_line('123 bold', bold=True)
+    expected = '\x1b[1m123 bold\x1b[0m'
+    assert result == expected
 
-    def test_print_bold(self):
-        result = consolor.get_line('123 bold', bold=True)
-        expected = '\x1b[1m123 bold\x1b[0m'
-        self.assertEqual(result, expected)
 
-    def test_print_underline(self):
-        result = consolor.get_line('123 underline', underline=True)
-        expected = '\x1b[4m123 underline\x1b[0m'
-        self.assertEqual(result, expected)
+def test_print_underline():
+    result = consolor.get_line('123 underline', underline=True)
+    expected = '\x1b[4m123 underline\x1b[0m'
+    assert result == expected
 
-    def test_get_bgcolor(self):
-        result = consolor.get_line('123 green bg', bgcolor=consolor.BgColor.Green)
-        expected = '\x1b[42;1m123 green bg\x1b[0m'
-        self.assertEqual(result, expected)
 
-    def test_get_color(self):
-        result = consolor.get_line('123 light green', color=consolor.Color.LightGreen)
-        expected = '\x1b[1;32m123 light green\x1b[0m'
-        self.assertEqual(result, expected)
+def test_get_bgcolor():
+    result = consolor.get_line('123 green bg', bgcolor=consolor.BgColor.Green)
+    expected = '\x1b[42;1m123 green bg\x1b[0m'
+    assert result == expected
 
-    def test_update_line(self):
-        for i in reversed(range(101)):
-            line = consolor.get_line('123%d' % i, update_line=True)
-            expected = '\x1b[2K\r%s%d\x1b[0m' % ('123', i)
-            self.assertEqual(line, expected)
 
-    def test_print_color(self):
-        with patch('tests.test_consolor.mockable_print') as mocked_print:
-            mockable_print(consolor.Color.Red, 'Red')
-            mockable_print('Red two')
-            mockable_print(consolor.Color.Reset, end='')
-            mockable_print('Not Red')
+def test_get_color():
+    result = consolor.get_line('123 light green',
+                               color=consolor.Color.LightGreen)
+    expected = '\x1b[1;32m123 light green\x1b[0m'
+    assert result == expected
 
-            mocked_print.assert_has_calls([call('\x1b[0;31m', 'Red'),
-                                           call('Red two'),
-                                           call('\x1b[0m', end=''),
-                                           call('Not Red')])
 
-    def test_print_concat_color(self):
-        with patch('tests.test_consolor.mockable_print') as mocked_print:
-            mockable_print(consolor.Color.Red, 'Red')
-            mockable_print('Red two')
-            mockable_print(consolor.Color.Blue, 'Blue')
-            mockable_print(consolor.Color.Reset, end='')
-            mockable_print('Not Blue')
+def test_update_line():
+    for i in reversed(range(101)):
+        line = consolor.get_line('123%d' % i, update_line=True)
+        expected = '\x1b[2K\r%s%d\x1b[0m' % ('123', i)
+        assert line == expected
 
-            mocked_print.assert_has_calls([call('\x1b[0;31m', 'Red'),
-                                           call('Red two'),
-                                           call('\x1b[0;34m', 'Blue'),
-                                           call('\x1b[0m', end=''),
-                                           call('Not Blue')])
 
-    def test_print_bgcolor(self):
-        with patch('tests.test_consolor.mockable_print') as mocked_print:
-            mockable_print(consolor.BgColor.Red, 'Red')
-            mockable_print('Red two', consolor.BgColor.Reset)
-            mockable_print('None')
+@patch('tests.test_consolor.mockable_print')
+def test_print_color(mocked_print):
+    mockable_print(consolor.Color.Red, 'Red')
+    mockable_print('Red two')
+    mockable_print(consolor.Color.Reset, end='')
+    mockable_print('Not Red')
 
-            mocked_print.assert_has_calls([call('\x1b[41;1m', 'Red'),
-                                           call('Red two', '\x1b[0m'),
-                                           call('None')])
+    mocked_print.assert_has_calls([call('\x1b[0;31m', 'Red'),
+                                   call('Red two'),
+                                   call('\x1b[0m', end=''),
+                                   call('Not Red')])
 
-    def test_print_concat_bgcolor(self):
-        with patch('tests.test_consolor.mockable_print') as mocked_print:
-            mockable_print(consolor.BgColor.Red, 'Red')
-            mockable_print('Red two')
-            mockable_print(consolor.BgColor.Cyan, 'None')
-            mockable_print(consolor.BgColor.Reset)
 
-            mocked_print.assert_has_calls([call('\x1b[41;1m', 'Red'),
-                                           call('Red two'),
-                                           call('\x1b[46;1m', 'None'),
-                                           call('\x1b[0m')])
+@patch('tests.test_consolor.mockable_print')
+def test_print_concat_color(mocked_print):
+    mockable_print(consolor.Color.Red, 'Red')
+    mockable_print('Red two')
+    mockable_print(consolor.Color.Blue, 'Blue')
+    mockable_print(consolor.Color.Reset, end='')
+    mockable_print('Not Blue')
+
+    mocked_print.assert_has_calls([call('\x1b[0;31m', 'Red'),
+                                   call('Red two'),
+                                   call('\x1b[0;34m', 'Blue'),
+                                   call('\x1b[0m', end=''),
+                                   call('Not Blue')])
+
+
+@patch('tests.test_consolor.mockable_print')
+def test_print_bgcolor(mocked_print):
+    mockable_print(consolor.BgColor.Red, 'Red')
+    mockable_print('Red two', consolor.BgColor.Reset)
+    mockable_print('None')
+
+    mocked_print.assert_has_calls([call('\x1b[41;1m', 'Red'),
+                                   call('Red two', '\x1b[0m'),
+                                   call('None')])
+
+
+@patch('tests.test_consolor.mockable_print')
+def test_print_concat_bgcolor(mocked_print):
+    mockable_print(consolor.BgColor.Red, 'Red')
+    mockable_print('Red two')
+    mockable_print(consolor.BgColor.Cyan, 'None')
+    mockable_print(consolor.BgColor.Reset)
+
+    mocked_print.assert_has_calls([call('\x1b[41;1m', 'Red'),
+                                   call('Red two'),
+                                   call('\x1b[46;1m', 'None'),
+                                   call('\x1b[0m')])
